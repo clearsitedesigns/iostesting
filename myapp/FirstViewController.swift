18ee204034f586ba09b180920c66ec553d1f9208
Preston M <preston@clearsitedesigns.com>
2014-10-15 09:13:05 -0500

    Initial commit - or whatever text you'd prefer

---
+++ myapp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  myapp
+//
+//  Created by imaginethepoet on 10/15/14.
+//  Copyright (c) 2014 imaginethepoet. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
